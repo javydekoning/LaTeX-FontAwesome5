6246d138f4d998edc20aef2d20d85e7239331ce1
Javy de Koning <javydekoning@gmail.com>
2017-12-11 22:58:59 +0100

    Pushing code changes for ShareLatex support + example

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # LaTeX-FontAwesome5
-Repo to prepare Font Awesome for use in LaTeX
+How to use FontAwesome 5 otf fonts with XeLaTeX
+
+1. Save the fonts folder, fontawesome.sty and fontawesomesymbols.tex to your project folder.
+2. Put \usepackage{fontawesome} into your preamble.
+3. Done!
+
+## Want to compile a new version yourself? 
+
+1. Download latest font from FontAwesome website.
+2. Extract latest otf fonts and icons.json metadata from zip.
+2. Run createDefinitions.js with nodejs to generate new fontawesomesymbols.tex
+
+#Using ShareLatex.com? 
+Don't forget to set compiler to XeLaTex
--- createDefinitions.js
+++ createDefinitions.js
@@ -1,66 +1,88 @@
 fs = require('fs')
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1); 
-  }
-
-fs.readFile('C:\\Users\\jdekoning.IPSOFT\\Downloads\\fontawesome-free-5.0.1\\fontawesome-free-5.0.1\\advanced-options\\metadata\\icons.json', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var obj = (JSON.parse(data));
-
-  results = ''; 
-  for (i in obj) {
-    /*
-      A valid definition looks like this in LaTeX: ​​​​​
-      \def\faWPBeginner{\FAB\symbol{"F297}}​​​​​
-      Let's build that 
-    */
-
-    //define start of string '\def\fa' 
-    var start = '\\def\\fa' 
-
-    //Define the label
-    //Sanatizing the label name for use of Font Awesome 5 in LaTeX
-    var name = obj[i].label.               // add fa Prefix  
-        split(/\-|\s/).map(function(entry) {        // Replace white space and -
-            return capitalizeFirstLetter(entry);    // Camel Case instead
-        }).join('').                                // Join back to string
-        replace(/'|\(|\)/g,'').                     // Remove ', ( and )
-        replace('1/4','Quarter').                   // Remove illegal numbers 
-        replace('1/2','Half').                      // And slashes
-        replace('3/4','ThreeQuarter').
-        replace('500','FiveHundred').
-        replace('2','Two').
-        replace('3','Three').
-        replace('4','Four').
-        replace('5','Five').
-        replace('8','Eight')
+function sanitizeLabel(label) {
+    var result = label
+        .split(/\-|\s|\./)                   // split ' ','.' and '-' to array 
+        .map(s => s.charAt(0).toUpperCase() // camelCase each word in array
+                + s.slice(1)                // rest of string
+        ) 
+        .join('')                           // Join new array back to string
+        .replace(/'|\(|\)/g,'')             // Remove ', ( and )
+        .replace('1/4','Quarter')           // Replace illegal numbers 
+        .replace('1/2','Half')              // And slashes
+        .replace('3/4','ThreeQuarter')
+        .replace('500','FiveHundred')
+        .replace('2','Two')
+        .replace('3','Three')
+        .replace('4','Four')
+        .replace('5','Five')
+        .replace('8','Eight')    
 
     //If we encounter a number, the sanitization needs to be updated. 
-    if (name.match(/\d/)) {
+    if (result.match(/\d/)) {
         throw('unreplaced number in: ' + str)
     }
-    
-    //Some icons have solid and regular fonts, we loop over styles to add both. 
-    for (j = 0; j < obj[i].styles.length; j++) {
-        //Set unique suffix if we have more then one style
-        if (obj[i].styles.length > 1) {
-            var suffix = obj[i].styles[j] == 'brands' ? 'B' : obj[i].styles[j] == 'solid' ? 'S' : obj[i].styles[j] == 'regular' ? 'R' : 'N'
-        } else {
-            //Use empty string if we have just one.
-            var suffix = '';
-        }
 
-        //Set font prefix FAB, FAS or FAR (Brands, Solid, Regular)
-        var prefix = obj[i].styles[j] == 'brands' ? '\\FAB' : obj[i].styles[j] == 'solid' ? '\\FAS' : obj[i].styles[j] == 'regular' ? '\\FAR' : 'N'
+    return result; 
+}
 
-        //Concatenate the string
-        var str = start + name + suffix + '{' + prefix + '\\symbol{"' + obj[i].unicode.toUpperCase() + '}}'
-        results += str + '\n'
+function parseIconsFile(obj) {
+    results = []; 
+    for (i in obj) {
+      /*
+        A valid definition looks like this in LaTeX: ​​​​​
+        \def\faWPBeginner{\FAB\symbol{"F297}}​​​​​
+        Let's build that 
+      */
+  
+      //define start of string '\def\fa' 
+      var start = '\\def\\fa' 
+  
+      //Sanatizing the label name for use of Font Awesome 5 in LaTeX
+      //e.g. 'red river' -> 'RedRiver', 'html5' -> htmlFive
+      var name = sanitizeLabel(obj[i].label)
+      
+      //Some icons have solid and regular fonts, we loop over styles to add both.  
+      // e.g. ​​​​​\def\faWindowRestoreS and ​\def\faWindowRestoreR
+      //      Solid               ^ and Regular             ^
+      for (j = 0; j < obj[i].styles.length; j++) {
+          var suffix = obj[i].styles.length == 1     ? ''  :     // No suffix req
+                       obj[i].styles[j] == 'brands'  ? 'B' :     // B  suffix req
+                       obj[i].styles[j] == 'solid'   ? 'S' :     // S  suffix req
+                       obj[i].styles[j] == 'regular' ? 'R' :''   // R  suffix req
+  
+          //Set font prefix FAB, FAS or FAR (Brands, Solid, Regular)
+          var prefix = obj[i].styles[j] == 'brands'  ? '\\FAB' : 
+                       obj[i].styles[j] == 'solid'   ? '\\FAS' : 
+                       obj[i].styles[j] == 'regular' ? '\\FAR' : ''
+  
+          //create symbol e.g. \symbol{"F0AD}
+          var symbol = '\\symbol{"' + obj[i].unicode.toUpperCase() + '}'
+          
+          //Concatenate the string
+          var str = start + name + suffix + '{' + prefix + symbol + '}'
+          
+          //add to results
+          results.push(str);
+      }
     }
-  }
+    return results;
+} 
 
-  console.log(results)
+function writeSymbolsFile(content) {
+    fs.writeFile(".\\fontawesomesymbols.tex", content, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+}
+
+//read & parse file. 
+fs.readFile('.\\icons.json', 'utf8', function (err,data) {
+  if (err) {throw(err)}
+
+  var results = parseIconsFile(JSON.parse(data));
+  writeSymbolsFile(results.join('\n'));
 });

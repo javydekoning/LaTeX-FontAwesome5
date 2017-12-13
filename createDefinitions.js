d591ac2d1eafc61b12bd20a21b639d4936699073
Javy de Koning <javydekoning@gmail.com>
2017-12-13 16:28:19 +0100

    Example files

--- createDefinitions.js
+++ createDefinitions.js
@@ -1,8 +1,12 @@
 fs = require('fs')
 
 function sanitizeLabel(label) {
+    if (label == 'Büromöbel-Experte GmbH & Co. KG.') {
+        return 'BuromobelExperte'
+    }
+
     var result = label
-        .split(/\-|\s|\./)                   // split ' ','.' and '-' to array 
+        .split(/\-|\s|\.|,/)                   // split ' ','.' and '-' to array 
         .map(s => s.charAt(0).toUpperCase() // camelCase each word in array
                 + s.slice(1)                // rest of string
         ) 
@@ -29,42 +33,44 @@ function sanitizeLabel(label) {
 function parseIconsFile(obj) {
     results = []; 
     for (i in obj) {
-      /*
-        A valid definition looks like this in LaTeX: ​​​​​
-        \def\faWPBeginner{\FAB\symbol{"F297}}​​​​​
-        Let's build that 
-      */
-  
-      //define start of string '\def\fa' 
-      var start = '\\def\\fa' 
-  
-      //Sanatizing the label name for use of Font Awesome 5 in LaTeX
-      //e.g. 'red river' -> 'RedRiver', 'html5' -> htmlFive
-      var name = sanitizeLabel(obj[i].label)
-      
-      //Some icons have solid and regular fonts, we loop over styles to add both.  
-      // e.g. ​​​​​\def\faWindowRestoreS and ​\def\faWindowRestoreR
-      //      Solid               ^ and Regular             ^
-      for (j = 0; j < obj[i].styles.length; j++) {
-          var suffix = obj[i].styles.length == 1     ? ''  :     // No suffix req
-                       obj[i].styles[j] == 'brands'  ? 'B' :     // B  suffix req
-                       obj[i].styles[j] == 'solid'   ? 'S' :     // S  suffix req
-                       obj[i].styles[j] == 'regular' ? 'R' :''   // R  suffix req
-  
-          //Set font prefix FAB, FAS or FAR (Brands, Solid, Regular)
-          var prefix = obj[i].styles[j] == 'brands'  ? '\\FAB' : 
-                       obj[i].styles[j] == 'solid'   ? '\\FAS' : 
-                       obj[i].styles[j] == 'regular' ? '\\FAR' : ''
-  
-          //create symbol e.g. \symbol{"F0AD}
-          var symbol = '\\symbol{"' + obj[i].unicode.toUpperCase() + '}'
-          
-          //Concatenate the string
-          var str = start + name + suffix + '{' + prefix + symbol + '}'
-          
-          //add to results
-          results.push(str);
-      }
+        /*
+            A valid definition looks like this in LaTeX: ​​​​​
+            \def\faWPBeginner{\FAB\symbol{"F297}}​​​​​
+            Let's build that 
+        */
+    
+        //define start of string '\def\fa' 
+        var start = '\\def\\fa' 
+    
+        //Sanatizing the label name for use of Font Awesome 5 in LaTeX
+        //e.g. 'red river' -> 'RedRiver', 'html5' -> htmlFive
+
+        var name = sanitizeLabel(obj[i].label)
+        
+
+        //Some icons have solid and regular fonts, we loop over styles to add both.  
+        // e.g. ​​​​​\def\faWindowRestoreS and ​\def\faWindowRestoreR
+        //      Solid               ^ and Regular             ^
+        for (j = 0; j < obj[i].styles.length; j++) {
+            var suffix = obj[i].styles.length == 1     ? ''  :     // No suffix req
+                        obj[i].styles[j] == 'brands'  ? 'B' :     // B  suffix req
+                        obj[i].styles[j] == 'solid'   ? 'S' :     // S  suffix req
+                        obj[i].styles[j] == 'regular' ? 'R' :''   // R  suffix req
+    
+            //Set font prefix FAB, FAS or FAR (Brands, Solid, Regular)
+            var prefix = obj[i].styles[j] == 'brands'  ? '\\FAB' : 
+                        obj[i].styles[j] == 'solid'   ? '\\FAS' : 
+                        obj[i].styles[j] == 'regular' ? '\\FAR' : ''
+    
+            //create symbol e.g. \symbol{"F0AD}
+            var symbol = '\\symbol{"' + obj[i].unicode.toUpperCase() + '}'
+            
+            //Concatenate the string
+            var str = start + name + suffix + '{' + prefix + symbol + '}'
+            
+            //add to results
+            results.push(str);
+        }
     }
     return results;
 } 
